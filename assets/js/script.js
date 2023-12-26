b7a73001b75fd2fe4e23199cd6738d9cf270b512
Jordan Ferrario <108853449+JordanFerrario@users.noreply.github.com>
2023-12-26 01:14:52 -0800

    finished writing all 10 questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,6 +101,118 @@ function loadQuestion2() {
     choiceD.onclick = () => checkAnswer('choice-d');
 }
 
+function loadQuestion3() {
+    correctChoice = 'choice-c';
+    question.innerHTML = '3. Which of the following is a valid type of function in JavaScript?'
+    choiceA.innerHTML = 'A. Named function'
+    choiceB.innerHTML = 'B. Anonymous function'
+    choiceC.innerHTML = 'C. Both named and anonymous functions'
+    choiceD.innerHTML = 'D. None of the above'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion4() {
+    correctChoice = 'choice-b';
+    question.innerHTML = '4. How do you create a function in JavaScript?'
+    choiceA.innerHTML = 'A. function:myFunction()'
+    choiceB.innerHTML = 'B. function myFunction()'
+    choiceC.innerHTML = 'C. function = myFunction()'
+    choiceD.innerHTML = 'D. create myFunction()'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion5() {
+    correctChoice = 'choice-b';
+    question.innerHTML = '5. How can you add a comment in JavaScript?'
+    choiceA.innerHTML = 'A. $This is a comment'
+    choiceB.innerHTML = 'B. // This is a comment'
+    choiceC.innerHTML = 'C. &lt;!-- This is a comment --&gt;'
+    choiceD.innerHTML = 'D. ** This is a comment **'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion6() {
+    correctChoice = 'choice-d';
+    question.innerHTML = '6. What will the following code return: Boolean(10&gt;9)?'
+    choiceA.innerHTML = 'A. NaN'
+    choiceB.innerHTML = 'B. false'
+    choiceC.innerHTML = 'C. null'
+    choiceD.innerHTML = 'D. true'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion7() {
+    correctChoice = 'choice-d';
+    question.innerHTML = '7. How do you declare a JavaScript variable?'
+    choiceA.innerHTML = 'A. variable carName;'
+    choiceB.innerHTML = 'B. v carName;'
+    choiceC.innerHTML = 'C. var = carName;'
+    choiceD.innerHTML = 'D. var carName;'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion8() {
+    correctChoice = 'choice-a';
+    question.innerHTML = '8. Which event occurs when the user clicks on an HTML element?'
+    choiceA.innerHTML = 'A. onclick'
+    choiceB.innerHTML = 'B. onmouseover'
+    choiceC.innerHTML = 'C. onchange'
+    choiceD.innerHTML = 'D. onmouseclick'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion9() {
+    correctChoice = 'choice-b';
+    question.innerHTML = '9. Which operator is used to assign a value to a variable?'
+    choiceA.innerHTML = 'A. *'
+    choiceB.innerHTML = 'B. ='
+    choiceC.innerHTML = 'C. -'
+    choiceD.innerHTML = 'D. x'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
+function loadQuestion10() {
+    correctChoice = 'choice-c';
+    question.innerHTML = '10. What is the correct way to write a JavaScript array?'
+    choiceA.innerHTML = 'A. var team7 = "naruto", "sasuke", "sakura"'
+    choiceB.innerHTML = 'B. var team7 = ("naruto", "sasuke", "sakura")'
+    choiceC.innerHTML = 'C. var team7 = ["naruto", "sasuke", "sakura"]'
+    choiceD.innerHTML = 'D. var team7 = {"naruto", "sasuke", "sakura"}'
+
+    choiceA.onclick = () => checkAnswer('choice-a');
+    choiceB.onclick = () => checkAnswer('choice-b');
+    choiceC.onclick = () => checkAnswer('choice-c');
+    choiceD.onclick = () => checkAnswer('choice-d');
+}
+
 startQuiz()
 
 
